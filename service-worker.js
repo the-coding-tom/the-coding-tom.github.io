12e2b8705fba3b1b3088236d85798effd2fb9a61
Thomas Kensah <tom.kensah@gmail.com>
2020-07-14 17:27:35 +0000

    init

--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ad1c107adb84607a0e26abb59aa0d105.js"
+  "/precache-manifest.615afe37cf4a6b4adf84791ff084fdba.js"
 );
 
-workbox.core.setCacheNameDetails({prefix: "senseble-health"});
+workbox.core.setCacheNameDetails({prefix: "serenity-patient"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,5 +31,4 @@ workbox.core.setCacheNameDetails({prefix: "senseble-health"});
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
