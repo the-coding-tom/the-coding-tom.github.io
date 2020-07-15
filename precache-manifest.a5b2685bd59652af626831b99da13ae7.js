3a5127a8616d2d69422f08791aea1ccfc4d1fe62
Thomas Kensah <tom.kensah@gmail.com>
2020-07-15 09:58:03 +0000

    init

---
+++ precache-manifest.a5b2685bd59652af626831b99da13ae7.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10fe55265a1ab74194b2",
+    "url": "/css/about.fcf19f20.css"
+  },
+  {
+    "revision": "9cabb97c5f60099c2a9a",
+    "url": "/css/app.583149f4.css"
+  },
+  {
+    "revision": "572919650459b5596c8fbee922022b33",
+    "url": "/css/bot.css"
+  },
+  {
+    "revision": "a205038b0f690a27eb8b",
+    "url": "/css/chunk-vendors.4521b365.css"
+  },
+  {
+    "revision": "c231b17f5708aec0a1a0",
+    "url": "/css/details.e6975154.css"
+  },
+  {
+    "revision": "19b0b529b6a7f8a98e316b4ee231df49",
+    "url": "/img/Home.19b0b529.svg"
+  },
+  {
+    "revision": "4d7bdb03d31080c00ba789a0f5f766b1",
+    "url": "/img/bglogin.4d7bdb03.svg"
+  },
+  {
+    "revision": "b22016ec905f68359574b1a1f1270c2e",
+    "url": "/img/doctor.b22016ec.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f65a41f6ad50d4b3a80ca6c7cc8f1b43",
+    "url": "/img/logo.f65a41f6.svg"
+  },
+  {
+    "revision": "8e14e2b86377c0710bfe7b87ed5e331e",
+    "url": "/img/neurosurgery.8e14e2b8.svg"
+  },
+  {
+    "revision": "9882a35a2d8464d2cdb7b4565c2fd51f",
+    "url": "/img/serenity-logo.9882a35a.svg"
+  },
+  {
+    "revision": "f700c53eb81cbd913a647081f1e9895e",
+    "url": "/img/sofa.f700c53e.svg"
+  },
+  {
+    "revision": "39444cb5ca2e6c49d17a0113bfbc9250",
+    "url": "/img/virus.39444cb5.svg"
+  },
+  {
+    "revision": "9482cb94e917e9edf139b4e41382a6e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10fe55265a1ab74194b2",
+    "url": "/js/about.75b05247.js"
+  },
+  {
+    "revision": "9cabb97c5f60099c2a9a",
+    "url": "/js/app.c2139ce5.js"
+  },
+  {
+    "revision": "a205038b0f690a27eb8b",
+    "url": "/js/chunk-vendors.14ff1605.js"
+  },
+  {
+    "revision": "796f7010df05cf7ab302994a276af3d4",
+    "url": "/js/conversational-form.js"
+  },
+  {
+    "revision": "c231b17f5708aec0a1a0",
+    "url": "/js/details.d54135c8.js"
+  },
+  {
+    "revision": "d20b828c74e13f451156196c60045a94",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f02b1efa814b7999ba0f159ee3260a5f.js"
+  "/precache-manifest.a5b2685bd59652af626831b99da13ae7.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "serenity-patient"});
