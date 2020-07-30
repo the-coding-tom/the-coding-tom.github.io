53e4f64cc8bb49bcec76be0d4cb0c9a024c2627f
Thomas Kensah <tom.kensah@gmail.com>
2020-07-30 06:48:45 +0000

    pushed serenity patient portal

---
+++ precache-manifest.060c2455ecc9b2970aa71f6e09561ae9.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0a0f4ad6ff93461af12",
+    "url": "/css/about.5e593344.css"
+  },
+  {
+    "revision": "a60041d1e329918688cd",
+    "url": "/css/app.d79d3bce.css"
+  },
+  {
+    "revision": "572919650459b5596c8fbee922022b33",
+    "url": "/css/bot.css"
+  },
+  {
+    "revision": "cdec3f91a8474712f675",
+    "url": "/css/chunk-vendors.4521b365.css"
+  },
+  {
+    "revision": "21ddef930dd26992a109",
+    "url": "/css/details.661784f3.css"
+  },
+  {
+    "revision": "c3faa681164c5ced720f5af4bce017dd",
+    "url": "/css/jquery.sweet-modal.min.css"
+  },
+  {
+    "revision": "28949c38a32a5481fd25d2c771b00590",
+    "url": "/icon.png"
+  },
+  {
+    "revision": "19b0b529b6a7f8a98e316b4ee231df49",
+    "url": "/img/Home.19b0b529.svg"
+  },
+  {
+    "revision": "4d7bdb03d31080c00ba789a0f5f766b1",
+    "url": "/img/bglogin.4d7bdb03.svg"
+  },
+  {
+    "revision": "b22016ec905f68359574b1a1f1270c2e",
+    "url": "/img/doctor.b22016ec.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f65a41f6ad50d4b3a80ca6c7cc8f1b43",
+    "url": "/img/logo.f65a41f6.svg"
+  },
+  {
+    "revision": "8e14e2b86377c0710bfe7b87ed5e331e",
+    "url": "/img/neurosurgery.8e14e2b8.svg"
+  },
+  {
+    "revision": "9882a35a2d8464d2cdb7b4565c2fd51f",
+    "url": "/img/serenity-logo.9882a35a.svg"
+  },
+  {
+    "revision": "f700c53eb81cbd913a647081f1e9895e",
+    "url": "/img/sofa.f700c53e.svg"
+  },
+  {
+    "revision": "4feb78c8ddfc34549524b09c5ef0583e",
+    "url": "/img/sunrise.4feb78c8.svg"
+  },
+  {
+    "revision": "39444cb5ca2e6c49d17a0113bfbc9250",
+    "url": "/img/virus.39444cb5.svg"
+  },
+  {
+    "revision": "e2c5ea9f89a295cf4993c5d4cf7213e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0a0f4ad6ff93461af12",
+    "url": "/js/about.545abe26.js"
+  },
+  {
+    "revision": "a60041d1e329918688cd",
+    "url": "/js/app.cc60cc90.js"
+  },
+  {
+    "revision": "cdec3f91a8474712f675",
+    "url": "/js/chunk-vendors.51d2f9ca.js"
+  },
+  {
+    "revision": "796f7010df05cf7ab302994a276af3d4",
+    "url": "/js/conversational-form.js"
+  },
+  {
+    "revision": "a4f87c68cb53d35abcb5072db422528b",
+    "url": "/js/create-demo-room.js"
+  },
+  {
+    "revision": "21ddef930dd26992a109",
+    "url": "/js/details.842401e5.js"
+  },
+  {
+    "revision": "dc5e7f18c8d36ac1d3d4753a87c98d0a",
+    "url": "/js/jquery-3.5.1.min.js"
+  },
+  {
+    "revision": "a6b97b67503d1150dbc4b457fb6b7124",
+    "url": "/js/jquery.sweet-modal.min.js"
+  },
+  {
+    "revision": "5ed41118be20e43a15b7c4e78cffc264",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,103 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
+importScripts("/precache-manifest.060c2455ecc9b2970aa71f6e09561ae9.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+// service-worker.js
 
-importScripts(
-  "/precache-manifest.500dd666d375d0f54312fac498db6028.js"
-);
+workbox.core.setCacheNameDetails({ prefix: 'd4' })
+    //Change this value every time before you build
+const LATEST_VERSION = 'v0.5'
+self.addEventListener('activate', (event) => {
+    // console.log(`%c ${LATEST_VERSION} `, 'background: #ddd; color: #e8e8e8')
+    if (caches) {
+        caches.keys().then((arr) => {
+            arr.forEach((key) => {
+                if (key.indexOf('d4-precache') < -1) {
+                    caches.delete(key).then(() => console.log(`%c Cleared ${key}`, 'background: #333; color: #ff0000'))
+                } else {
+                    caches.open(key).then((cache) => {
+                        cache.match('version').then((res) => {
+                            if (!res) {
+                                cache.put('version', new Response(LATEST_VERSION, { status: 200, statusText: LATEST_VERSION }))
+                            } else if (res.statusText !== LATEST_VERSION) {
+                                caches.delete(key).then(() => console.log(`%c Cleared Cache ${LATEST_VERSION}`, 'background: #333; color: #ff0000'))
+                            } else console.log(`%c Great you have the latest version ${LATEST_VERSION}`, 'background: #333; color: #00ff00')
+                        })
+                    })
+                }
+            })
+        })
+    }
+})
 
-workbox.core.setCacheNameDetails({prefix: "serenity-patient"});
+workbox.skipWaiting()
+workbox.clientsClaim()
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
+self.__precacheManifest = [].concat(self.__precacheManifest || [])
+workbox.precaching.suppressWarnings()
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+//install new service worker when ok, then reload page
+self.addEventListener("message", msg => {
+    if(msg.data.action == 'skipWaiting'){
+        self.skipWaiting()
+    }
+})
+
+self.addEventListener('notificationclick', event => {
+    let targetUrl = new URL('/');
+    event.waitUntil(async function() {
+      const allClients = await clients.matchAll({
+        includeUncontrolled: true,
+        type: 'window'
+      });
+  
+      let requestClient;
+  
+      // Let's see if we already have a request window open:
+      for (const client of allClients) {
+        const url = new URL(client.url);
+  
+        if (url.host === targetUrl.host && 'focus' in client) {
+          // Excellent, let's use it!
+        //   Update URL
+          client.navigate('/');
+        //   Focus existing window
+          client.focus()
+          requestClient = client;
+          break;
+        }
+      }
+  
+      // If we didn't find an existing request window,
+      // open a new one:
+      if (!requestClient) {
+        requestClient = await clients.openWindow('/');
+      }
+  
+      // Message the client:
+      requestClient.postMessage("Your request status updated!");
+    }());
+  });
+
+
+self.addEventListener('push', function(event) {
+    // console.log('[Service Worker] Push Received.');
+    // console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+  
+    const eventInfo = event.data.text();
+    const data = JSON.parse(eventInfo);
+    const title = data.head || 'New Notification 🕺🕺';
+    const body = data.body || 'This is default content. Your notification didn\'t have one 🙄🙄';
+    const options = {
+        body: body,
+        icon: '<%= BASE_URL %>favicon.ico',
+        vibrate: [200, 100, 200, 100, 200, 100, 400],
+        badge: '<%= BASE_URL %>favicon.ico'
+    };
+    // console.log(eventInfo)
+  
+    const notificationPromise = self.registration.showNotification(title, options);
+event.waitUntil(notificationPromise);
+  });
+
+
